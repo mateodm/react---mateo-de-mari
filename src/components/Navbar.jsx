5940c4332cfd6058612ff4c7f4377e2b14b61362
Mateo DM <104037425+mateodm@users.noreply.github.com>
2023-01-24 22:21:10 -0300

    arreglo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar_background navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">E-Commerce</a>
+                <a className="navbar-brand" href="#">E-Commerce React</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
